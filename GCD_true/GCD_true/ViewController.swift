d7cd6e48d108c9e9f2f3e6430acaa8473287f242
pazrvien <yjaewoong@naver.com>
2020-09-29 14:34:53 +0900

    비동기 처리 후

---
+++ GCD_true/GCD_true/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD_true
+//
+//  Created by 윤재웅 on 2020/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
